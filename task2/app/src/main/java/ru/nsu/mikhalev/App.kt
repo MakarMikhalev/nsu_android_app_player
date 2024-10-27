70710b15d157aa636147c45c408dcc58a94fbf7f
MakarMikhalev <nataliamikhalevaaa@mail.ru>
2024-10-27 16:00:50 +0700

    Delete task2 directory

--- task2/app/src/main/java/ru/nsu/mikhalev/App.kt
+++
@@ -1,8 +0,0 @@
-package ru.nsu.mikhalev
-
-import android.app.Application
-import ru.nsu.mikhalev.service.SongService
-
-class App : Application() {
-    val songService = SongService()
-}
--- task2/app/src/main/java/ru/nsu/mikhalev/model/Song.kt
+++
@@ -1,9 +0,0 @@
-package ru.nsu.mikhalev.model
-
-data class Song(
-    val id: Long,
-    val name: String,
-    val author: String,
-    val photo: Int,
-    var isPlayed: Boolean
-)
